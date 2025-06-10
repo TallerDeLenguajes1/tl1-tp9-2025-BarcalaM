39a732829cdc39b870ad4dd3b8e365573a68c762
Gastón Gutiérrez Llomplat <gastong7337@gmail.com>
2025-06-10 16:11:45 -0300

    avanzando con el punto uno

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola mundo");
+﻿Console.WriteLine("Hola mundo");
+
+using System.IO;
+using System;
+
+class Program
+{
+     static void Main(string[] args)
+    {
+        string? path;
+        string[] carpetas, files;
+        do
+        {
+            Console.WriteLine("Ingresar el path deseado: ");
+            path = Console.ReadLine();
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("EL directorio no existe. ");
+            }
+}
